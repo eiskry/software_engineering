999d7191d472492b3f3ef0ded1369bc1a62f7582
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-10-26 11:52:52 +0900

    edit

---
+++ week2/src/week2/TreeTest.java
@@ -0,0 +1,18 @@
+package week2;
+import java.util.Iterator;
+
+public class TreeTest {
+	public static void main(String[] args) {
+		Tree root = new Tree("A", 
+	                             new Tree("B",
+	                                      new Tree("C", null, null),
+	                                      new Tree("D", null, null)),
+	                             new Tree("E",
+	                                      new Tree("F", null, null),
+	                                      null));
+	        Iterator<Tree> i = root.iterator();
+	        while (i.hasNext()) {
+	            System.out.println(i.next().label);
+	        }
+	    }
+}
