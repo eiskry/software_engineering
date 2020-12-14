57b83902b53a71489d25205b63b819f3dc71bc90
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-12-14 19:16:34 +0900

    task8

--- task7/src/task7/TelephoneNumber.java
+++ task7/src/task7/TelephoneNumber.java
@@ -1,5 +1,13 @@
 package task7;
 
 public class TelephoneNumber {
-
+	private String areaCode;
+	
+	public String getAreaCode() {
+		return areaCode;
+	}
+	
+	public void setAreaCode(String areaCode) {
+		this.areaCode = areaCode;
+	}
 }
